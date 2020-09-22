59d10aa6f5d1bafa52bbc4fd7c49f63737962b16
Sazid <initialsaremsu@gmail.com>
2020-09-23 01:08:23 +0600

    Search now uses views instead of tables

--- faculty/search/includes/search.inc.php
+++ faculty/search/includes/search.inc.php
@@ -12,100 +12,130 @@ if (isset($_SESSION['userId']) && $_SESSION['userId']!== "") {
 			echo "<p>Please insert a search term first</p>";
 		}
 		else if(preg_match("~[0-9]~", $key) == 1){
-			$sql = "SELECT Id, SectionName FROM sections WHERE FacultyId = :fid";
+			$sql = "SELECT * FROM faculty_".$facultyId."_students where academicid like :key";
 			$stmt = oci_parse($conn, $sql);
 			if (!$stmt) {
 				echo "Failed to retrieve data";
 				exit();
 			}
 			else{
-				oci_bind_by_name($stmt, ':fid', $facultyId);
+				oci_bind_by_name($stmt, ':key', $key);
 				oci_execute($stmt);
-				$nrows = oci_fetch_all($stmt, $result, null, null, OCI_FETCHSTATEMENT_BY_ROW);
-				if($nrows == 0){
-					echo "<p>You have no students as of now.";
-				}
-				else{
-					echo '<table class=student-table>';
+
+				echo '<table class=student-table>';
 					echo '<tr><th>Academic ID</th><th>Name</th><th>Section</th></tr>';
 					oci_execute($stmt);
 					while (($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
-						$sql2 = "SELECT AcademicId, FirstName, LastName FROM users where AcademicId like :key AND UserType = 1 AND id in
-						(select StudentId FROM SectionStudents where SectionId = :sid)";
-						$stmt2 = oci_parse($conn, $sql2);
-						if (!$stmt2) {
-							echo "Failed to retrieve data";
-							exit();
-						}
-						else{
-							oci_bind_by_name($stmt2, ':key', $key);
-							oci_bind_by_name($stmt2, ':sid', $row['ID']);
-							oci_execute($stmt2);
+						echo '<tr><td>'.$row['ACADEMICID'].'</td><td>'.$row['FIRSTNAME'].' '.$row['LASTNAME'].'</td><td>'.$row['SECTIONNAME'].'</td></tr>';				
+						// $sql2 = "SELECT AcademicId, FirstName, LastName FROM users where AcademicId like :key AND UserType = 1 AND id in
+						// (select StudentId FROM SectionStudents where SectionId = :sid)";
+						// $stmt2 = oci_parse($conn, $sql2);
+						// if (!$stmt2) {
+						// 	echo "Failed to retrieve data";
+						// 	exit();
+						// }
+						// else{
+						// 	oci_bind_by_name($stmt2, ':key', $key);
+						// 	oci_bind_by_name($stmt2, ':sid', $row['ID']);
+						// 	oci_execute($stmt2);
 							
-							while (($row2 = oci_fetch_array($stmt2, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
-								echo '<tr><td>'.$row2['ACADEMICID'].'</td><td>'.$row2['FIRSTNAME'].' '.$row2['LASTNAME'].'</td><td>'.$row['SECTIONNAME'].'</td></tr>';				
-							}
-						}
+						// 	while (($row2 = oci_fetch_array($stmt2, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+						// 		echo '<tr><td>'.$row2['ACADEMICID'].'</td><td>'.$row2['FIRSTNAME'].' '.$row2['LASTNAME'].'</td><td>'.$row['SECTIONNAME'].'</td></tr>';				
+						// 	}
+						// }
 					}
 					echo '</table>';
-				}
 			}
+			// $sql = "SELECT Id, SectionName FROM sections WHERE FacultyId = :fid";
+			// $stmt = oci_parse($conn, $sql);
+			// if (!$stmt) {
+			// 	echo "Failed to retrieve data";
+			// 	exit();
+			// }
+			// else{
+			// 	oci_bind_by_name($stmt, ':fid', $facultyId);
+			// 	oci_execute($stmt);
+			// 	$nrows = oci_fetch_all($stmt, $result, null, null, OCI_FETCHSTATEMENT_BY_ROW);
+			// 	if($nrows == 0){
+			// 		echo "<p>You have no students as of now.";
+			// 	}
+			// 	else{
+			// 		echo '<table class=student-table>';
+			// 		echo '<tr><th>Academic ID</th><th>Name</th><th>Section</th></tr>';
+			// 		oci_execute($stmt);
+			// 		while (($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+			// 			$sql2 = "SELECT AcademicId, FirstName, LastName FROM users where AcademicId like :key AND UserType = 1 AND id in
+			// 			(select StudentId FROM SectionStudents where SectionId = :sid)";
+			// 			$stmt2 = oci_parse($conn, $sql2);
+			// 			if (!$stmt2) {
+			// 				echo "Failed to retrieve data";
+			// 				exit();
+			// 			}
+			// 			else{
+			// 				oci_bind_by_name($stmt2, ':key', $key);
+			// 				oci_bind_by_name($stmt2, ':sid', $row['ID']);
+			// 				oci_execute($stmt2);
+							
+			// 				while (($row2 = oci_fetch_array($stmt2, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+			// 					echo '<tr><td>'.$row2['ACADEMICID'].'</td><td>'.$row2['FIRSTNAME'].' '.$row2['LASTNAME'].'</td><td>'.$row['SECTIONNAME'].'</td></tr>';				
+			// 				}
+			// 			}
+			// 		}
+			// 		echo '</table>';
+			// 	}
 		}
 		else{
-			$sql = "SELECT Id, SectionName FROM sections WHERE FacultyId = :fid";
+			$sql = "SELECT * FROM faculty_".$facultyId."_students where firstname like :key or lastname like :key";
 			$stmt = oci_parse($conn, $sql);
 			if (!$stmt) {
 				echo "Failed to retrieve data";
 				exit();
 			}
 			else{
-				oci_bind_by_name($stmt, ':fid', $facultyId);
+				oci_bind_by_name($stmt, ':key', $key);
 				oci_execute($stmt);
-				$nrows = oci_fetch_all($stmt, $result, null, null, OCI_FETCHSTATEMENT_BY_ROW);
-				if($nrows == 0){
-					echo "<p>You have no students as of now.";
-				}
-				else{
-					echo '<table class=student-table>';
+
+				echo '<table class=student-table>';
 					echo '<tr><th>Academic ID</th><th>Name</th><th>Section</th></tr>';
 					oci_execute($stmt);
 					while (($row = oci_fetch_array($stmt, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
-						$sql2 = "SELECT AcademicId, FirstName, LastName FROM users where (FirstName like :key OR LastName like :key) AND UserType = 1 AND id in
-						(select StudentId FROM SectionStudents where SectionId = :sid)";
-						$stmt2 = oci_parse($conn, $sql2);
-						if (!$stmt2) {
-							echo "Failed to retrieve data";
-							exit();
-						}
-						else{
-							oci_bind_by_name($stmt2, ':key', $key);
-							oci_bind_by_name($stmt2, ':sid', $row['ID']);
-							oci_execute($stmt2);
-							while (($row2 = oci_fetch_array($stmt2, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
-								echo '<tr><td>'.$row2['ACADEMICID'].'</td><td>'.$row2['FIRSTNAME'].' '.$row2['LASTNAME'].'</td><td>'.$row['SECTIONNAME'].'</td></tr>';				
-							}
-						}
+						echo '<tr><td>'.$row['ACADEMICID'].'</td><td>'.$row['FIRSTNAME'].' '.$row['LASTNAME'].'</td><td>'.$row['SECTIONNAME'].'</td></tr>';				
+						// $sql2 = "SELECT AcademicId, FirstName, LastName FROM users where AcademicId like :key AND UserType = 1 AND id in
+						// (select StudentId FROM SectionStudents where SectionId = :sid)";
+						// $stmt2 = oci_parse($conn, $sql2);
+						// if (!$stmt2) {
+						// 	echo "Failed to retrieve data";
+						// 	exit();
+						// }
+						// else{
+						// 	oci_bind_by_name($stmt2, ':key', $key);
+						// 	oci_bind_by_name($stmt2, ':sid', $row['ID']);
+						// 	oci_execute($stmt2);
+							
+						// 	while (($row2 = oci_fetch_array($stmt2, OCI_ASSOC+OCI_RETURN_NULLS)) != false) {
+						// 		echo '<tr><td>'.$row2['ACADEMICID'].'</td><td>'.$row2['FIRSTNAME'].' '.$row2['LASTNAME'].'</td><td>'.$row['SECTIONNAME'].'</td></tr>';				
+						// 	}
+						// }
 					}
 					echo '</table>';
 				}
+				
+				/*$sql = "UPDATE attendances SET Entry = ? WHERE Id = ?";
+				$stmt = mysqli_stmt_init($conn);
+				
+				if (!mysqli_stmt_prepare($stmt, $sql)) {
+					echo "Attendance update failed: sqlerror";
+					exit();
+				}
+				else{
+					mysqli_stmt_bind_param($stmt, "ii", $attEntry, $attId);
+					mysqli_stmt_execute($stmt);
+					
+					echo "changed: ".$attId." to ".$attEntry;
+					exit();
+				}*/		
 			}
 		}
-		
-		/*$sql = "UPDATE attendances SET Entry = ? WHERE Id = ?";
-		$stmt = mysqli_stmt_init($conn);
-		
-		if (!mysqli_stmt_prepare($stmt, $sql)) {
-			echo "Attendance update failed: sqlerror";
-			exit();
-		}
-		else{
-			mysqli_stmt_bind_param($stmt, "ii", $attEntry, $attId);
-			mysqli_stmt_execute($stmt);
-			
-			echo "changed: ".$attId." to ".$attEntry;
-			exit();
-		}*/		
-	}
 	else{
 		echo "Attendance update failed: no post";
 	}
